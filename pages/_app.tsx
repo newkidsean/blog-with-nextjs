6a8308a430bbdf33d1a8e0f10fe0b506f9b86801
sean-onuii <sean1.onuii@gmail.com>
2022-05-14 19:00:38 +0900

    feat: rtk 기본 store & hook 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import GlobalStyle from '../src/styles/GlobalStyle';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 }
